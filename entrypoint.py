8703c759af47424d6546c2b45befb3f1b1511dcc
Takashi Uno <euno.eng@gmail.com>
2022-09-21 20:22:18 +0900

    fix : run test

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,18 @@
-print("hi")
+import argparse
+import subprocess
+import sys
 import os
 
 ls_file_name = os.listdir()
 
 print(ls_file_name)
+
+result = subprocess.run(
+    "checkmake -h",
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True,
+)
+print(result.stdout)
+print(result.stderr)
